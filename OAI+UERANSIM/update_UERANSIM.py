2f8fb624d5ed8d2f3720fbe61ea51373d7649de4
massi <massinissa.aitaba@gmail.com>
2025-02-26 15:23:26 +0100

    p

--- OAI+UERANSIM/update_UERANSIM.py
+++ OAI+UERANSIM/update_UERANSIM.py
@@ -3,6 +3,10 @@ import os
 namespace='oai'
 ip_adress="10.244.0.1"
 network="cni0"
+dnn=["oai","oai2","oai3"]
+gnbfiles=["OAI-gnb.yaml","OAI-gnb2.yaml","OAI-gnb3.yaml"]
+uefiles=["OAI-ue.yaml","OAI-ue2.yaml","OAI-ue3.yaml"]
+sst=[1,128,130]
 
 retour=os.popen("kubectl get pods -n "+namespace).read() 
 Running=0
@@ -14,16 +18,21 @@ with open(r'OAI-gnb.yaml', 'r') as file:
     file.close()
 data = data.replace("xxx", str(ip_adress))
 data = data.replace("yyy", str(amf_ip))
-with open(r'UERANSIM/build/OAI-gnb.yaml', 'w') as file:
-    file.write(data)
-    file.close()
+for i in range(0,len(dnn)):
+    data = data.replace("zzz", sst[i])
+    with open(r'UERANSIM/build/'+gnbfiles[i], 'w') as file:
+        file.write(data)
+        file.close()
 #update OAI-ue file for UERANSIM 
 with open(r'OAI-ue.yaml', 'r') as file:
     data = file.read()
     file.close()
 data = data.replace("xxx", str(ip_adress))
-data = data.replace("yyy", namespace)
-with open(r'UERANSIM/build/OAI-ue.yaml', 'w') as file:
-    file.write(data)
-    file.close()
+
+for i in range(0,len(dnn)):
+    data = data.replace("yyy", dnn[i])
+    data = data.replace("zzz", sst[i])
+    with open(r'UERANSIM/build/'+uefiles[i], 'w') as file:
+        file.write(data)
+        file.close()
 print("UERANSIM files configuration updated")
