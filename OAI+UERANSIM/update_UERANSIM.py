fa7ce5a8a3421cb8ff6ce56557aa15d0bbe0ad20
massi <massinissa.aitaba@gmail.com>
2025-02-26 15:32:44 +0100

    p

--- OAI+UERANSIM/update_UERANSIM.py
+++ OAI+UERANSIM/update_UERANSIM.py
@@ -13,26 +13,25 @@ Running=0
 amf_ip=os.popen("kubectl get pod -n "+namespace+" $(kubectl get pods --namespace "+namespace+" -l "+"app.kubernetes.io/name=oai-amf"+" -o jsonpath="+"{.items[0].metadata.name}"+") --template '{{.status.podIP}}'").read()
 os.system("sudo ifconfig "+network+":"+str(1)+" "+str(ip_adress)+" up")
 #update OAI-gnb file for UERANSIM 
-with open(r'OAI-gnb.yaml', 'r') as file:
-    data = file.read()
-    file.close()
-data = data.replace("xxx", str(ip_adress))
-data = data.replace("yyy", str(amf_ip))
+data={}
 for i in range(0,len(dnn)):
-    data = data.replace("zzz", str(sst[i]))
+    with open(r'OAI-gnb.yaml', 'r') as file:
+        data[i] = file.read()
+        file.close()
+    data[i] = data[i].replace("xxx", str(ip_adress))
+    data[i] = data[i].replace("yyy", str(amf_ip))
+    data[i] = data[i].replace("zzz", str(sst[i]))
     with open(r'UERANSIM/build/'+gnbfiles[i], 'w') as file:
-        file.write(data)
+        file.write(data[i])
         file.close()
 #update OAI-ue file for UERANSIM 
-with open(r'OAI-ue.yaml', 'r') as file:
-    data = file.read()
-    file.close()
-data = data.replace("xxx", str(ip_adress))
-
-for i in range(0,len(dnn)):
-    data = data.replace("yyy", dnn[i])
-    data = data.replace("zzz", str(sst[i]))
+    with open(r'OAI-ue.yaml', 'r') as file:
+        data[i] = file.read()
+        file.close()
+    data[i] = data[i].replace("xxx", str(ip_adress))
+    data[i] = data[i].replace("yyy", dnn[i])
+    data[i] = data[i].replace("zzz", str(sst[i]))
     with open(r'UERANSIM/build/'+uefiles[i], 'w') as file:
-        file.write(data)
+        file.write(data[i])
         file.close()
 print("UERANSIM files configuration updated")
