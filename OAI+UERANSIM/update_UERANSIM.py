5501950be47eb8cc0175152a5788bc0c04fc4560
massi <massinissa.aitaba@gmail.com>
2025-03-03 12:05:22 +0100

    p

--- OAI+UERANSIM/update_UERANSIM.py
+++ OAI+UERANSIM/update_UERANSIM.py
@@ -1,7 +1,7 @@
 import os
 
 namespace='oai'
-ip_adress="10.244.0.1"
+ip_adress=["10.244.0.1","10.244.0.2","10.244.0.3"]
 network="cni0"
 dnn=["oai","oai2","oai3"]
 gnbfiles=["OAI-gnb.yaml","OAI-gnb2.yaml","OAI-gnb3.yaml"]
@@ -11,14 +11,16 @@ sst=[128,1,130]
 
 retour=os.popen("kubectl get pods -n "+namespace).read() 
 amf_ip=os.popen("kubectl get pod -n "+namespace+" $(kubectl get pods --namespace "+namespace+" -l "+"app.kubernetes.io/name=oai-amf"+" -o jsonpath="+"{.items[0].metadata.name}"+") --template '{{.status.podIP}}'").read()
-os.system("sudo ifconfig "+network+":"+str(1)+" "+str(ip_adress)+" up")
+
+
 #update OAI-gnb file for UERANSIM 
 data={}
 for i in range(0,len(dnn)):
     with open(r'OAI-gnb.yaml', 'r') as file:
         data[i] = file.read()
         file.close()
-    data[i] = data[i].replace("xxx", str(ip_adress))
+    os.system("sudo ifconfig "+network+":"+str(1)+" "+str(ip_adress[i])+" up")
+    data[i] = data[i].replace("xxx", str(ip_adress[i]))
     data[i] = data[i].replace("yyy", str(amf_ip))
     data[i] = data[i].replace("zzz", str(sst[i]))
     data[i] = data[i].replace("ttt", str(nci[i]))
@@ -29,7 +31,7 @@ for i in range(0,len(dnn)):
     with open(r'OAI-ue.yaml', 'r') as file:
         data[i] = file.read()
         file.close()
-    data[i] = data[i].replace("xxx", str(ip_adress))
+    data[i] = data[i].replace("xxx", str(ip_adress[i]))
     data[i] = data[i].replace("yyy", dnn[i])
     data[i] = data[i].replace("zzz", str(sst[i]))
     with open(r'UERANSIM/build/'+uefiles[i], 'w') as file:
